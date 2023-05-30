16cdfd626cc80129ccb9d82bf3a93b26526bb080
anapaaula <x.anapaulaa@gmail.com>
2023-05-30 00:59:58 -0300

    feat: book functionalities

---
+++ src/main/java/com/biblioteko/biblioteko/book/NewBookDTO.java
@@ -0,0 +1,28 @@
+package com.biblioteko.biblioteko.book;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class NewBookDTO {
+
+    private String title;
+
+    private String author;
+
+    private String gender;
+    
+    private Integer edition;
+
+    private String synopsis;
+    
+    private Float rating;
+
+    private String photo;
+
+    private Integer pages;
+
+    private String accessLink;
+
+}
